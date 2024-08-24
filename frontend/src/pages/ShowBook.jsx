e1a2a4953dba0cfdaf523e209c5c37b7bcf788ed
Raj Patel <112432655+rajpat243@users.noreply.github.com>
2024-08-24 18:39:13 -0400

    Update ShowBook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`https://online-book-library-backend.onrender.com`)
+      .get(`http://localhost:5555/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
