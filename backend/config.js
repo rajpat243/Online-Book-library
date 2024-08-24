5d94fcde02f03b069381e7dd9225460d391aad04
Raj Patel <112432655+rajpat243@users.noreply.github.com>
2024-08-24 17:51:12 -0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = process.env.PORT || 5555;
-export const mongoDBURL = 'mongodb+srv://rajpat243:Raj428300@bookstore-mern.m9fidl5.mongodb.net/?retryWrites=true&w=majority';
+export const mongoDBURL = 'mongodb+srv://rajpat243:Raj428300@bookstore-mern.m9fidl5.mongodb.net/?retryWrites=true&w=majority&appName=BookStore-MERN';
 
