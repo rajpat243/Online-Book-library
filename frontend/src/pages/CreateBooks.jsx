2d27f7ad481fa5467a63f5dbba0e74ca66db45e9
Raj Patel <112432655+rajpat243@users.noreply.github.com>
2024-08-24 18:11:41 -0400

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('https://online-book-library-backend.onrender.com', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
